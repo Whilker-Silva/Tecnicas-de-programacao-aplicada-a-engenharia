ce26c3f4393b7b4a07b4406d2462ec029b9712e0
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-04-20 13:28:56 -0300

    semana 1

---
+++ AULAS PRÁTICAS/Semana 1/Exercicio2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        float investimentoJoao;
+        float investimentoPedro;
+
+        System.out.printf("Entre com o valor investido pelo Joao: ");
+        investimentoJoao = entrada.nextFloat();
+
+        System.out.printf("Entre com o valor investido pelo Pedro: ");
+        investimentoPedro = entrada.nextFloat();
+
+        int cont = 0;
+        while(investimentoJoao < investimentoPedro){
+
+            //calculo rendimento
+
+            cont++;
+        }
+
+        System.out.printf("Quantidade de meses necessarios: %d\n", cont);
+
+        entrada.close();
+    }
+}
---
+++ AULAS PRÁTICAS/Semana 1/Exercicio3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    int a;
+
+    float b;
+  }
+}
