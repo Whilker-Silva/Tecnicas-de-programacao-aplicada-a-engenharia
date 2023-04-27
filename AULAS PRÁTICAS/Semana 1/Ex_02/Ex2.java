75f8fde01b34847853e342ef1fd24d0156e0bc61
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-04-27 01:22:13 -0300

    Exercício 3 - semana 1

--- AULAS PRÁTICAS/Semana 1/Ex_02/Ex2.java
+++ AULAS PRÁTICAS/Semana 1/Ex_02/Ex2.java
@@ -1,29 +1,36 @@
 package Ex_02;
+
 import java.util.Scanner;
 
 public class Ex2 {
     public static void main(String[] args) {
 
+        // Criando obejto para realizar a leitura da entrada padrão
         Scanner entrada = new Scanner(System.in);
 
-        float investimentoJoao;
-        float investimentoPedro;
+        // Declação variáveis
+        double investimentoJoao;
+        double investimentoPedro;
 
-        System.out.printf("Entre com o valor investido pelo Joao: ");
-        investimentoJoao = entrada.nextFloat();
+        // Realiza a intereção com o terminal para ser informado valores investidos
+        System.out.print("Entre com o valor investido pelo Joao:");
+        investimentoJoao = entrada.nextDouble();
+        System.out.print("Entre com o valor investido pelo Pedro:");
+        investimentoPedro = entrada.nextDouble();
 
-        System.out.printf("Entre com o valor investido pelo Pedro: ");
-        investimentoPedro = entrada.nextFloat();
+        
+        int meses = 0; // variável para contagem de meses
 
-        int cont = 0;
-        while(investimentoJoao < investimentoPedro){
+        /*Enquanto valor investido por joão for menor 
+        que o valor investido por pedro*/
+        while (investimentoJoao < investimentoPedro) {
 
-            //calculo rendimento
-
-            cont++;
+            investimentoJoao += investimentoJoao * 0.025;
+            investimentoPedro += investimentoPedro * 0.005;
+            meses++;
         }
 
-        System.out.printf("Quantidade de meses necessarios: %d\n", cont);
+        System.out.printf("\nQuantidade de meses necessarios: %d\n", meses);
 
         entrada.close();
     }
