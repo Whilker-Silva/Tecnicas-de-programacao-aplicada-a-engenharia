3cd5646878d5c02ff4de418aa53ff81bfedd9d27
Whilker Henrique dos Santos Silva <77589533+Whilker-Silva@users.noreply.github.com>
2023-07-13 10:23:01 -0300

    .

---
+++ aulas_praticas/semana13/aula2/ex1/Principal.java
@@ -0,0 +1,7 @@
+package semana13.aula2.ex1;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ aulas_praticas/semana13/aula2/ex1/Sanduiche.java
@@ -0,0 +1,10 @@
+package semana13.aula2.ex1;
+
+public class Sanduiche {
+
+    private String nome;
+
+    public Sanduiche(String nome) {
+        this.nome = nome;
+    }
+}
